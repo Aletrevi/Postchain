6dd41a1b196f0c73ea52c4c99d1f35b9710ec685
Trevi <alessio.trevisan.2@studenti.unipd.it>
2022-03-14 00:28:22 +0100

    main frontend implementation

--- web-interface/src/app/app.module.ts
+++ web-interface/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { PostComponent } from './post/post.component';
+import { PostContainerComponent } from './post-container/post-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    PostComponent,
+    PostContainerComponent
   ],
   imports: [
     BrowserModule,
