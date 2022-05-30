265e47dbcf0f163052242643854f02dc59edc262
nursaadat <SNursultan@dar.kz>
2022-05-30 19:28:09 +0200

    local tests

--- loadTests/common/posts.py
+++ loadTests/common/posts.py
@@ -2,32 +2,27 @@ from json import JSONDecodeError
 import random
 import string
 
-posts_base_url = '/api/posts'
+posts_base_url = '/posts'
 
 def list_posts(self):
-    return self.client.get(posts_base_url, catch_response=True)
+    return self.client.get('http://localhost:9232/posts')
 
 def get_post_by_id(self):
      with self.client.get(posts_base_url, catch_response=True) as response:
-        try:
-            response = response.json()
-            post = random.choice(response)
-            return self.client.get(
-                f'{posts_base_url}/{post["_id"]}', name=f"{posts_base_url}/booking_id", catch_response=True)
-        except JSONDecodeError:
-            response.failure("Response could not be decoded as JSON")
-        except KeyError:
-            response.failure(
-                "Response did not contain expected key booking.id")
+        response = response.json()
+        print(response)
+        post = random.choice(response)
+        return self.client.get(
+            f'{posts_base_url}/{post["_id"]}', name=f"{posts_base_url}/booking_id", catch_response=True)
 
-def add_post(self):
-    post_data = generate_random_post_data(self)
-    return self.client.post(posts_base_url, json=post_data)
+# def add_post(self):
+#     post_data = generate_random_post_data(self)
+#     return self.client.post(posts_base_url, json=post_data)
 
-def generate_random_post_data(self):
-    letters = string.ascii_letters
-    author = ''.join(random.choice(letters) for i in range(10))
-    title = ''.join(random.choice(letters) for i in range(10))
-    body = ''.join(random.choice(letters) for i in range(10))
+# def generate_random_post_data(self):
+#     letters = string.ascii_letters
+#     author = ''.join(random.choice(letters) for i in range(10))
+#     title = ''.join(random.choice(letters) for i in range(10))
+#     body = ''.join(random.choice(letters) for i in range(10))
 
-    return {"author": author, "title": title, "body": body}
+#     return {"author": author, "title": title, "body": body}
--- loadTests/locustfile.py
+++ loadTests/locustfile.py
@@ -1,5 +1,5 @@
 from locust import HttpUser, task, between
-import common.posts as postsssss #TODO add it in common folder
+import common.posts as posts #TODO add it in common folder
 # import common.blockchain_interactor as bc #TODO add it in common folder
 # import common.checker as checker #TODO add it in common folder
 # import common.orchestrator as orchestrator #TODO add it in common folder
@@ -7,20 +7,14 @@ import common.posts as postsssss #TODO add it in common folder
 class User(HttpUser):
     wait_time = between(1,3)
 
-    # TEST
-    @task
-    def my_task(self):
-        print("User instance (%r) executing my_task" % self)
-
-    # POSTS
     @task
     def list_posts(self):
-        postsssss.list_posts(self)
+        posts.list_posts(self)
 
     @task
     def get_post_by_id(self):
-        postsssss.get_post_by_id(self)
+        posts.get_post_by_id(self)
 
-    @task
-    def add_post(self):
-        postsssss.add_post(self)
+    # @task
+    # def add_post(self):
+    #     postsssss.add_post(self)
